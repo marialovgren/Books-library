d1a0f2bcf3a124fcf0b2fa9fb06e8365741f79a8
marialovgren <marialovgren86@hotmail.com>
2022-08-16 13:28:09 +0200

    moved request to get books into a custom hook called useBooks

---
+++ src/hooks/useBooks.js
@@ -0,0 +1,10 @@
+// denna hooken gör själva queryn som hämtar böckerna. Syftet är att bara ha en query-nyckel för att bara göra en request. 
+
+import { useQuery } from "react-query";
+import BooksAPI from '../services/BooksAPI'
+
+const useBooks = () => {
+    return useQuery('books', BooksAPI.getBooks)
+}
+
+export default useBooks
--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,13 +1,12 @@
 import Container from 'react-bootstrap/Container'
-import { useQuery } from 'react-query'
-import BooksAPI from '../services/BooksAPI'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import BookCard from '../components/BookCard';
 import WarningAlert from '../components/alerts/WarningAlert';
+import useBooks from '../hooks/useBooks';
 
 const BooksPage = () => {
-	const { isLoading, isError, error, data } = useQuery('books', BooksAPI.getBooks)
+	const { isLoading, isError, error, data } = useBooks()
 
 	console.log(data)
 
