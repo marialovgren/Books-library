ba72c90b38b26c7f16ac542679a304b8f1534709
marialovgren <marialovgren86@hotmail.com>
2022-08-23 10:08:14 +0200

    Setup of React Hook Form on CreateAuthor form.

--- src/components/forms/CreateAuthorForm.jsx
+++ src/components/forms/CreateAuthorForm.jsx
@@ -1,22 +1,31 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { useForm } from 'react-hook-form'
 
 const CreateAuthorForm = () => {
-	const onSubmit = e => {
-		e.preventDefault()
-		console.log("would create author")
+	const { register, handleSubmit } = useForm()
+
+	const onCreateAuthor = data => {
+		console.log("would create author with data:", data)
 	}
 
 	return (
-		<Form onSubmit={onSubmit}>
+		<Form onSubmit={handleSubmit(onCreateAuthor)}>
 			<Form.Group className='mb-3' controlId='name'>
 				<Form.Label>Author Name</Form.Label>
-				<Form.Control type='text' placeholder='Astrid Lindgren' />
+				<Form.Control
+					{...register('name')}
+					type='text'
+					placeholder='Astrid Lindgren'
+				/>
 			</Form.Group>
 
 			<Form.Group className='mb-3' controlId='date_of_birth'>
 				<Form.Label>Date of Birth</Form.Label>
-				<Form.Control type='date' />
+				<Form.Control
+					{...register('date_of_birth')}
+					type='date'
+				/>
 			</Form.Group>
 
 			<Button variant='success' type='submit'>Create</Button>
